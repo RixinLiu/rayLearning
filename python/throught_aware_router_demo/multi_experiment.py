ab3077ddad815641c32344123b85c3e1caec0361
Rixin <rixin.liu.coder@gmail.com>
2025-03-28 01:18:29 -0500

    sync with l4-2gpus

--- python/throught_aware_router_demo/multi_experiment.py
+++ python/throught_aware_router_demo/multi_experiment.py
@@ -61,6 +61,21 @@ def run_router(strategy):
             processes.append(process)  # 将子进程添加到全局列表中
         
         print(f"Router started with strategy: {strategy}")
+        time.sleep(5)
+        run_benchmark(args.num_prompts, args.concurrency)  # 启动基准测试
+
+
+def run_benchmark(num_prompts, concurrency):
+    cmd = f"""python3 -m bench_serving --backend vllm --host 127.0.0.1 --port 8000 \
+    --dataset-name sharegpt --num-prompts {num_prompts} --sharegpt-output-len 256 --max-concurrency {concurrency} \
+    --dataset-path ./long_short_pattern_sharegpt_requests.json"""
+    
+    # 启动路由器并捕获输出
+    with open(f"benchmark.log", "w") as log_file:
+        process = subprocess.Popen(cmd, shell=True, stdout=log_file, stderr=log_file)
+        processes.append(process)  # 将子进程添加到全局列表中
+    
+    print(f"Benchmark started with {num_prompts} prompts and concurrency {concurrency}.")
 
 def signal_handler(sig, frame):
     """捕获 Ctrl+C 信号并终止所有子进程"""
