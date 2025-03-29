e5e77ca6779101d9bca00c08d1318cfff8c4b185
Rixin <rixin.liu.coder@gmail.com>
2025-03-29 03:10:04 -0500

    sync with l4-2gpus

--- python/throught_aware_router_demo/multi_experiment.py
+++ python/throught_aware_router_demo/multi_experiment.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     os.makedirs(experiment_root_dir, exist_ok=True)
 
     # 路由策略和实验次数
-    strategies = ["tokens", "round_robin", "pow_2"]
+    strategies = ["round_robin", "tokens", "pow_2"]
     num_experiments = 1
 
     for strategy in strategies:
