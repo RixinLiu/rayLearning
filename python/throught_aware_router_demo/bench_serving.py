5d0101636736a05f4b965479c0e0d06dd8249475
Rixin <rixin.liu.coder@gmail.com>
2025-04-01 03:30:58 -0500

    sync with t4-2gpus

--- python/throught_aware_router_demo/bench_serving.py
+++ python/throught_aware_router_demo/bench_serving.py
@@ -172,9 +172,8 @@ async def async_request_openai_completions(
         st = time.perf_counter()
         most_recent_timestamp = st
         try:
-            print(payload)
             async with session.post(
-                url=api_url, json=payload, headers=headers, chunked=False,
+                url=api_url, json=payload, headers=headers
             ) as response:
                 if response.status == 200:
                     async for chunk_bytes in response.content:
